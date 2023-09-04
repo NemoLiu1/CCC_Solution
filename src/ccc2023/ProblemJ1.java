66866d39ea9a56cb0dd22126636b7fa61d5f7d91
NemoLiu1 <111040925+NemoLiu1@users.noreply.github.com>
2023-09-04 09:34:57 +0800

    Update ProblemJ1.java

--- src/ccc2023/ProblemJ1.java
+++ src/ccc2023/ProblemJ1.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class ProblemJ1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int p = in.nextInt();
-        int c = in.nextInt();
-        int points = (p * 50) + (c * -10);
-        if (p > c){
+        int addition = in.nextInt();
+        int subtraction = in.nextInt();
+        int points = (addition * 50) + (subtraction * -10);
+        if (addition > subtraction){
             points += 500;
         }
         System.out.println(points);
